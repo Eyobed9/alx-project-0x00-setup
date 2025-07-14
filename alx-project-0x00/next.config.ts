f7a6a7cbc29ad034c9f3be0750fa09126dcc1d66
Eyobed <eyobedteshome@gmail.com>
2025-07-14 17:39:50 +0300

    basic routing is done

--- alx-project-0x00/next.config.ts
+++ alx-project-0x00/next.config.ts
@@ -1,16 +1,19 @@
-import type { NextConfig } from "next";
-import type { Configuration } from 'webpack';
+import type { NextConfig } from 'next';
+import type { Configuration} from 'webpack';
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	webpackDevMiddleware(config:Configuration) {
-		config.watchOptions = {
-			poll: 1000,
-			aggregateTimeout: 300,
-		};
-		return config;
-	},
-	reactStrictMode: true,
+  webpack(config: Configuration, { dev }) {
+    if (dev && config.watchOptions) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+        ignored: /node_modules/,
+      };
+    }
+	
+    return config;
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
